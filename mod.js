d01bdb93db770754ed3dd465d9a6493fc89bb439
Cbass92 <140918855+sebastian-92@users.noreply.github.com>
2024-07-24 14:30:38 -0400

    Create mod.js

---
+++ mod.js
@@ -0,0 +1,20 @@
+        if (typeof CCSE === 'undefined') {
+            Game.LoadMod('https://klattmose.github.io/CookieClicker/CCSE.js');
+        }
+
+        BakingSoda.launch = function() {
+            BakingSoda.name = "Baking Soda";
+            BakingSoda.isLoaded = 1;
+            console.log("working")
+        };
+
+        if (!BakingSoda.isLoaded) {
+            if (CCSE && CCSE.isLoaded) {
+                BakingSoda.launch();
+            } else {
+                if (!CCSE) var CCSE = {};
+                if (!CCSE.postLoadHooks) CCSE.postLoadHooks = [];
+                CCSE.postLoadHooks.push(BakingSoda.launch);
+            }
+        }
+    }
